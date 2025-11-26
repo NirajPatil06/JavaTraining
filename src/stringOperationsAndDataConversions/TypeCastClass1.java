c97a4b48831f06917d23b6cb6885e4496e71ada2
NirajPatil06 <nirajvyas1997@gmail.com>
2025-11-26 23:57:57 +0530

    changes

--- src/stringOperationsAndDataConversions/TypeCastClass1.java
+++ src/stringOperationsAndDataConversions/TypeCastClass1.java
@@ -5,6 +5,12 @@ public class TypeCastClass1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		  int [] arr = {1,4,9};
+		    for(int i=0;i<arr.length-1;i++){
+		        for(int j=arr[i]+1;j<arr[i+1];j++){
+		            System.out.println("Missing number is : "+ j);
+		        }
+		    }
 	}
 
 }
